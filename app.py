9ab61a06712917de395d4fcdd9fe9861ddf5eb45
nandagopalmb-1 <nandagopalmb1@gmail.com>
2026-01-17 18:48:19 +0530

    some additions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 
-# ------------------ URL CHECK FUNCTIONS ------------------
+
 
 def is_ip_address(url):
     ip_pattern = re.compile(
@@ -85,8 +85,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
